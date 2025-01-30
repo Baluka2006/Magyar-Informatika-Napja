5cac97d43be3e5e6d4b0385f9bdf86465f9bb5aa
Baluka2006 <k.balazs.2006@gmail.com>
2025-01-30 16:57:45 +0100

    Form és JS

--- scripts.js
+++ scripts.js
@@ -3,7 +3,34 @@ function kuldes() {
     let eletkor = document.getElementById("years").value;
     let email = document.getElementById("email").value;
     let jegyek = document.getElementById("tickets").value;
-    let szallasEtel = document.adatok.etelSzallas;
+    let szallasEtel = document.adatok.etelSzallas.value;
+    let hibaUzenetek = "";
 
-    console.log(szallasEtel)
+    if (nev === "") {
+        hibaUzenetek += "A név megadása kötelező!<br>";
+    }
+    
+    if (eletkor === "" || isNaN(eletkor)) {
+        hibaUzenetek += "Az életkor megadása kötelező!<br>";
+    } else if (parseInt(eletkor) < 18) {
+        hibaUzenetek += "Csak 18 év felettiek vehetnek részt a konferencián!<br>";
+    }
+    
+    if (email === "") {
+        hibaUzenetek += "Az e-mail cím megadása kötelező!<br>";
+    }
+    
+    if (jegyek === "" || isNaN(jegyek)) {
+        hibaUzenetek += "A jegyek számának megadása kötelező!<br>";
+    }
+    
+    let hibaSzekció = document.getElementById("errorContainer");
+    if (!hibaSzekció) {
+        hibaSzekció = document.createElement("p");
+        hibaSzekció.id = "errorContainer";
+        document.forms["adatok"].appendChild(hibaSzekció);
+    }
+    
+    hibaSzekció.innerHTML = hibaUzenetek;
+    hibaSzekció.style.color = "red";
 }
